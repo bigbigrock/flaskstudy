d54ab6537bc36c566005bf90d061f062eaa5e0c9
bigbigrock <1562028916@qq.com>
2020-11-06 09:48:52 +0800

    1106早上

--- demos/hello/app.py
+++ demos/hello/app.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 @app.route('/hello')
 def index():
     return '<h1>hello hahah </h1>' \
-           '<h1>加油吧<h1>'
+           '<h1>加油吧<h1>'\
+           '<h1>hahahhahaha</h1>'\
+           '<p>hdhdhdh</p>'
+
