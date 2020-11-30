62f9dcbc8bf18cfd4379d26a847f3251ef64979d
bigbigrock <1562028916@qq.com>
2020-11-30 18:35:45 +0800

    20201130

--- demos/http/app.py
+++ demos/http/app.py
@@ -1,4 +1,5 @@
-from flask import Flask,request,redirect,url_for,abort,make_response,jsonify
+from flask import Flask,request,redirect,url_for,abort,make_response,jsonify,session
+import os
 
 app = Flask(__name__)
 # @app.route('/hello',methods=['GET','POST'])
@@ -8,9 +9,9 @@ app = Flask(__name__)
 #     return '<h1>hello,%s!</h1>'%name #插入到返回值中
 
 
-@app.route('/goback/<int:year>')
-def go_back(year):
-    return '<p>Welcome to %d!</p>'%(2018 - year)
+# @app.route('/goback/<int:year>')
+# def go_back(year):
+#     return '<p>Welcome to %d!</p>'%(2018 - year)
 
 
 # @app.route('/colors/<any(blue,white,red):color>')
@@ -23,29 +24,29 @@ def go_back(year):
 # def three_colors(color):
 #     return '<p>Love is patient and kind.Love is not jealous or boastful or proud or rude.</p>'
 
-@app.before_request
-def do_something():
-    pass  #这里的代码会在每个请求处理前执行
+# @app.before_request
+# def do_something():
+#     pass  #这里的代码会在每个请求处理前执行
 
 # @app.route('/hello')
 # def hello():
 #     ...
 #     return '',302,{'Location':'http://www.example.com'}
 
-# #重定向
+#重定向
 # @app.route('/hello')
 # def hello():
 #     return redirect('http://www.example.com')
 
-#重定向到其他视图函数
-@app.route('/hi')
-def hi():
-    ...
-    return redirect(url_for('hello'))  #重定向到hello
-
-@app.route('/404')
-def not_found():
-    abort(404)
+# #重定向到其他视图函数
+# @app.route('/hi')
+# def hi():
+#     ...
+#     return redirect(url_for('hello'))  #重定向到hello
+#
+# @app.route('/404')
+# def not_found():
+#     abort(404)
 
 
 # @app.route('/foo')
@@ -54,12 +55,39 @@ def not_found():
 #     response.mimetype = 'text/plain'
 #     return response
 
-@app.route('/foo')
-def foo():
-    return jsonify(name='grey li',gender='male')
+# @app.route('/foo')
+# def foo():
+#     return jsonify(name='grey li',gender='male')
+#
+# @app.route('/set/<name>')
+# def set_cookie(name):
+#     response = make_response(redirect(url_for('hello')))
+#     response.set_cookie('name',name)
+#     return response
 
-@app.route('/set/<name>')
-def set_cookie(name):
-    response = make_response(redirect(url_for('hello')))
-    response.set_cookie('name',name)
+app.secret_key = os.getenv('SECRET_KEY','secret string')
+
+@app.route('/login')
+def login():
+    session['logged_in'] = True #写入session
+    return redirect(url_for('hello'))
+
+from flask import request,session
+@app.route('/')
+@app.route('/hello')
+def hello():
+    name = request.args.get('name')
+    if name is None:
+        name = request.cookies.get('name','Human')
+        response = '<h1>Hello,%s!</h1>'%name
+    #根据用户认证状态返回不同的内容
+    if 'logged_in' in session:
+        response += '[Authenticated]'
+    else:
+        response += '[Not Authenticated]'
     return response
+
+
+
+
+
