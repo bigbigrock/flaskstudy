880208af82bfe672816a9424fe5aff5a1ab3e4c5
bigbigrock <1562028916@qq.com>
2020-12-15 20:21:19 +0800

    '20201215'

--- demos/form/app.py
+++ demos/form/app.py
@@ -1,9 +1,14 @@
-from flask import Flask,render_template,request,redirect,url_for,flash
+from flask import Flask,render_template,request,redirect,url_for,flash,session,send_from_directory
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
-from forms import LoginForm
+from forms import LoginForm,UploadForm,MultiUploadForm,RichTextForm
+import os
+import uuid
+from flask_wtf.csrf import validate_csrf
+from wtforms import ValidationError
 
+from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 # app_ctx = app.app_context()
@@ -14,6 +19,9 @@ app.secret_key = "secret string"
 #     form = LoginForm()
 #     return render_template('bootstrap.html',form=form)
 
+@app.route('/',methods=['GET','POST'])
+def index():
+    return  render_template('index.html')
 
 @app.route('/basic',methods=['get','post'])
 def basic():
@@ -33,3 +41,92 @@ class MyBaseForm(FlaskForm):
 class HelloForm(MyBaseForm):
     name = StringField('name',validators=[DataRequired()])
     submit = SubmitField
+
+# #实例化表单类
+# @app.route('/upload',methods=['GET','POST'])
+# def upload():
+#     form = UploadForm()
+#     ...
+#     return render_template('upload.html',form=form)
+
+#处理上传文件
+app.config['UPLOAD_PATH'] = os.path.join(app.root_path,'uploads')
+@app.route('/upload',methods = ['GET','POST'])
+def upload():
+    form = UploadForm()
+    if form.validate_on_submit():
+        f = form.photo.data
+        filename = random_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_PATH'],filename))
+        flash('Upload sucess')
+        session['filenames'] = [filename]
+        return redirect(url_for('show_image'))
+    return render_template('upload.html',form=form)
+
+#重命名文件
+def random_filename(filename):
+    ext = os.path.splitext(filename)[1]
+    new_name = uuid.uuid4().hex + ext
+    return new_name
+
+@app.route('/uploads/<path:filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'],filename)
+
+@app.route('/uploaded-images')
+def show_images():
+    return render_template('uploded.html')
+
+#处理多文件上传
+@app.route('/multi-upload',methods=['GET','POST'])
+def multi_upload():
+    form = MultiUploadForm()
+    if request.method == "POST":
+        filenames = []
+        #验证CSRF令牌
+        try:
+            validate_csrf(form.csrf_token.data)
+        except ValidationError:
+            flash('CSRF token error')
+            return redirect(url_for('multi_upload'))
+        #检查文件是否存在
+        if 'photo' not in request.files:
+            flash('This field is required')
+            return redirect(url_for('multi_upload'))
+
+
+        for f in request.files.getlist('photo'):
+            #检查文件类型
+            if f and allowed_file(f.filename):
+                filename = random_filename(f.filename)
+                f.save(os.path.join(
+                    app.config['UPLOAD_PATH'],filename
+                ))
+                filenames.append(filename)
+            else:
+                flash('Invalid file type')
+                return redirect(url_for('multi_upload'))
+        flash('Upload sucess')
+        session['filenames']=filename
+        return redirect(url_for('show_images'))
+    return render_template('upload.html',form=form)
+
+#验证文件类型
+app.config['ALLOWED_EXTENSIONS']=['png','jpg','jpeg','gif']
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+#富文本
+app.config['CKEDITOR_SERVE_LOCAL']=True
+ckeditor = CKEditor(app)
+@app.route('/ckeditor',methods=['GET','POST'])
+def integrate_ckeditor():
+    form = RichTextForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        flash('Your post is published')
+        return render_template('post.html',title=title,body=body)
+    return render_template('ckeditor.html',form=form)
+
+
--- demos/form/forms.py
+++ demos/form/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField,IntegerField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,IntegerField,MultipleFileField
 from wtforms.validators import DataRequired,Length,ValidationError
+from flask_wtf.file import FileField,FileRequired,FileAllowed
+
+from flask_ckeditor import CKEditorField
 
 class LoginForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired()])
@@ -8,11 +11,47 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Log in')
 
-
+#行内验证器
 class FortyTwoForm(FlaskForm):
     answer = IntegerField('The Number')
     submit = SubmitField()
     def validate_answer(form,field):
         if field.data != 42:
             raise ValidationError("Must be 42")
+#全局验证器
+def is_42(form,field):
+    if field.data !=42:
+        raise ValidationError('Must be 42')
 
+class FortyTwoForm(FlaskForm):
+        answer = IntegerField('The Number',validators=[is_42])
+        submit = SubmitField()
+
+#工厂函数形式的全局验证器示例
+def is_42(message=None):
+    if message is None:
+        message = 'Must be 42'
+    def _is_42(form,field):
+        if field.data != 42:
+            raise ValidationError(message)
+    return _is_42
+
+class FortyTwoForm(FlaskForm):
+    answer = IntegerField('The Number',validators=[is_42()])
+    submit = SubmitField
+
+#创建上传表单
+class UploadForm(FlaskForm):
+    photo = FileField('Upload Image',validators=[FileRequired(),FileAllowed(['jpg','jpeg','png','gif'])])
+    submit = SubmitField()
+
+#多文件上传
+class MultiUploadForm(FlaskForm):
+    photo = MultipleFileField('Upload Image',validators=[DataRequired()])
+    submit = SubmitField()
+
+#文章表单
+class RichTextForm(FlaskForm):
+    title = StringField('title',validators=[DataRequired(),Length(1,50)])
+    body = CKEditorField('Body',validators=[DataRequired()])
+    submit = SubmitField('Publish')
